bac86ed47f97ea0d0da145f172dbe4eb84721787
pablitocalvo <pablitocalvo@users.noreply.github.com>
2019-12-14 08:15:14 +0100

    Update buildtinix.sh

--- buildtinix.sh
+++ buildtinix.sh
@@ -8,3 +8,4 @@ pendest=${output:0:3}
 
 echo
 #https://www.tecmint.com/progress-monitor-check-progress-of-linux-commands/
+#https://stackoverflow.com/questions/2853803/how-to-echo-shell-commands-as-they-are-executed
